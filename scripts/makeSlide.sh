68ca73a940b6c65a85480ec16b220e54e9939a8f
Romain Madar <romain.madar@cern.ch>
2018-07-30 15:24:00 +0200

    Modifying script with possible fonts and to be compatible with last version of pandoc

--- scripts/makeSlide.sh
+++ scripts/makeSlide.sh
@@ -9,7 +9,7 @@ if [ $# -ne 2 ]; then
     fi
 
 # Beamer slides
-pandoc ${1} -o ${2} --to beamer --template ../templates/slides_template.tex --slide-level=2 \
+pandoc -N -s ${1} -o ${2} --to beamer --template ${PANDOC_TEMPLATES}/slides_template.tex --slide-level=2 \
        -V theme="CambridgeUS" -V colortheme="seagull" -V fonttheme="structurebold" -V fontfamily="libertine" \
        -V colorlinks\
        -V toc\
