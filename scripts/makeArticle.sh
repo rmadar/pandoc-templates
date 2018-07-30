e3d7499afd130b909e60296210663e37e87f2256
Romain Madar <romain.madar@cern.ch>
2018-07-30 15:11:53 +0200

    Testing some new possible font

--- scripts/makeArticle.sh
+++ scripts/makeArticle.sh
@@ -9,7 +9,14 @@ if [ $# -ne 2 ]; then
 fi
 
 # PDF via latex
-pandoc -N ${1} -o ${2} --template ../templates/document_template.tex \
-       --variable geometry="a4paper, total={6in, 9in}" -V linestretch="1.2" -V fontsize="11pt"\
-       --listings  --highlight-style kate\
+pandoc -N -s ${1} -o ${2} --template ${PANDOC_TEMPLATES}/document_template.tex \
+       --variable geometry="a4paper, total={6in,9in}"\
+       -V linestretch="1.2" -V fontsize="11pt"\
+       --listings --highlight-style kate\
        --toc
+
+
+## Other nice font combination
+# -V linestretch="1.0" -V fontsize="11pt" -V fontfamily="kmath,kerkis" 
+# -V linestretch="1.2" -V fontsize="11pt" -V fontfamily="cmbright"
+# -V linestretch="1.2" -V fontsize="11pt" -V fontfamily="kurier"
