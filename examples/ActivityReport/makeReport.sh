20984933007cd37e9c05cafaf55e1777ac652770
Romain Madar <romain.madar@cern.ch>
2018-08-02 01:15:37 +0200

    Adding ieee style even if links are missing

--- examples/ActivityReport/makeReport.sh
+++ examples/ActivityReport/makeReport.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # PDF via latex
-pandoc -N -s RapportActivite.md -o RapportActivite.pdf --bibliography biblio.bib --filter pandoc-citeproc --csl springer-socpsych-brackets.csl --metadata link-citations=true -V linestretch="1.3" -V fontsize="11pt" -V language="francais" -V fontfamily="gillius" -V fontoption="default" --toc --toc-depth=2 -V display-abstract -V geometry="a4paper, total={6in,9in}" --template ${PANDOC_TEMPLATES}/document_template.tex
+pandoc -N -s RapportActivite.md -o RapportActivite.pdf --bibliography biblio.bib --filter pandoc-citeproc --csl ieee.csl --metadata link-citations=true -V linestretch="1.3" -V fontsize="11pt" -V language="francais" -V fontfamily="gillius" -V fontoption="default" --toc --toc-depth=2 -V display-abstract -V geometry="a4paper, total={6in,9in}" --template ${PANDOC_TEMPLATES}/document_template.tex
        
 # HTML
 pandoc -N -s RapportActivite.md -o RapportActivite.html --css ${PANDOC_TEMPLATES}/webstyle_template.css\
