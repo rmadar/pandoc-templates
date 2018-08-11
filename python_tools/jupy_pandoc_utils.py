bb7bba9692027d9c2cc35ce38999a6fc5c1484a4
Romain Madar <romain.madar@cern.ch>
2018-08-11 15:23:09 +0200

    Making a first stable version of jupy_pandoc_utils

--- python_tools/jupy_pandoc_utils.py
+++ python_tools/jupy_pandoc_utils.py
@@ -1,24 +1,41 @@
 
+import os
 from IPython.display import Markdown
 from tabulate import tabulate
 
-jupy_pandoc_COUNTER=0
+jupy_pandoc_COUNTERS={}
 
 def plt2md(figlabel,figcaption,figsize):
+    '''
+    Doc string to be written
+    '''
+    import matplotlib.pyplot as plt
     global jupy_pandoc_COUNTER
-    figname = figlabel+'_'+str(jupy_pandoc_COUNTER)+'.png'
-    plt.tight_layout()
+
+    if figlabel not in jupy_pandoc_COUNTERS:
+        jupy_pandoc_COUNTERS[figlabel]=0
+        
+    # Incrementation is needed to see the updated file in jupyter
+    # due to data caching in web browser. To avoid a large amount
+    # of png, the last one only is kept.
+    for i in range(0,jupy_pandoc_COUNTERS[figlabel]):
+        oldfilename=os.path.join(os.getcwd(),figlabel+'__n'+str(i)+'.png')
+        if (os.path.isfile(oldfilename)):
+            os.remove(oldfilename)
+            
+    figname = figlabel+'__n'+str(jupy_pandoc_COUNTERS[figlabel])+'.png'
     plt.savefig(figname)
     plt.savefig(figlabel+'.pdf')
-    strMD='![{}]({})'.format(figcaption,figname) +\
-        '{' + 'width={} #'.format(figsize) + figlabel + '}'
-    jupy_pandoc_COUNTER+=1
+    plt.close()
+    strMD='![{}]({})'.format(figcaption,figname) \
+        +'{' + 'width={} #'.format(figsize) + figlabel + '}'
+
+    jupy_pandoc_COUNTERS[figlabel]+=1
     return display(Markdown(strMD))
 
 
 def df2md(df):
-    return display(
-        Markdown(
-            tabulate(df, headers='keys',tablefmt='pipe')
-        )
-    )
+    '''
+    Doc string to be written
+    '''
+    return display( Markdown(tabulate(df, headers='keys',tablefmt='pipe') ))
