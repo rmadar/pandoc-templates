93b276013702b5faa663f080818301225f8dd23e
Romain Madar <romain.madar@gmail.com>
2018-08-12 22:33:12 +0200

    Using french as default language

--- examples/ActivityReport/makeReport.sh
+++ examples/ActivityReport/makeReport.sh
@@ -7,8 +7,9 @@ pandoc -N -s RapportActivite.md -o RapportActivite.pdf\
        -V fontfamily="gillius" -V fontoption="default"\
        --toc --toc-depth=2 -V display-abstract\
        -V geometry="a4paper, total={6in,9in}"\
-       --template ${PANDOC_TEMPLATES}/document_template.tex
-       
+       --template ${PANDOC_TEMPLATES}/document_template.tex\
+       -V language="francais"
+
 # HTML
 pandoc -N -s RapportActivite.md -o RapportActivite.html --css ${PANDOC_TEMPLATES}/webstyle_template.css\
        --bibliography biblio.bib --filter pandoc-citeproc --csl ${PANDOC_TEMPLATES}/biblio_style.csl --metadata link-citations=true\
