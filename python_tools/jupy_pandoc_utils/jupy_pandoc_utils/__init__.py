9d16d13ab47271a6b7d1661b72684e453d1c7679
Romain Madar <romain.madar@gmail.com>
2018-08-14 11:37:48 +0200

    Packaging jupy_pandoc_utils

---
+++ python_tools/jupy_pandoc_utils/jupy_pandoc_utils/__init__.py
@@ -0,0 +1,1 @@
+from .jupy_pandoc_utils import *
---
+++ python_tools/jupy_pandoc_utils/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jupy_pandoc_utils',
+      version='0.1',
+      description='Utils to properly export jupyter notebook in markdown with caption and labels for figures and tables',
+      packages=['jupy_pandoc_utils'],
+      zip_safe=False,
+      license='MIT',
+      author_email='romain.madar@gmail.com',
+      author='Romain Madar',
+)
