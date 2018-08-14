69e55c681a917148f66a8f66d28c7035f2154775
Romain Madar <romain.madar@gmail.com>
2018-08-14 11:40:34 +0200

    Add python package installation in the setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# Environment variables
 export PANDOC_TEMPLATES=`pwd`/templates
 export PANDOC_SCRIPTS=`pwd`/scripts
 export PANDOC_FILTERS=`pwd`/filters
+
+# Update the path
 PATH=$PATH:${PANDOC_SCRIPTS}
+
+# Locally install python helper packages
+cd python_tools/jupy_pandoc_utils/
+pip install -e .  --user
+cd -
